1f4993b82a219c3bb13e03b0ae8f2170fb007a3e
XHWang <57834871+xhwang22@users.noreply.github.com>
2024-09-25 16:30:00 +0800

    Update DUAL-step1.sh

--- code/bash/DUAL-step1.sh
+++ code/bash/DUAL-step1.sh
@@ -5,7 +5,7 @@ WORLD_SIZE=1 CUDA_VISIBLE_DEVICES=0,1,2,3 python step1/DUAL-step1.py   \
     --preference_type "chosen"
 
 WORLD_SIZE=1 CUDA_VISIBLE_DEVICES=0,1,2,3 python step1/DUAL-step1.py   \
-    --model_path "../model/SFT" \
+    --model_path "Liuwenhao2022/RAHF-SFT" \
     --data_path  "../data/ultrafeedback/rm" \
     --output_dir "../model/DUAL/bad" \
     --preference_type "rejected"
